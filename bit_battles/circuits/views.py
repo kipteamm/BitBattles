502ca7ae6d30c6b069013da50f2b6e1b4343cebf
kipteamm <toro.een@gmail.com>
2024-12-17 16:09:29 +0100

    Added truthtables to circuit views

--- bit_battles/circuits/views.py
+++ bit_battles/circuits/views.py
@@ -1,4 +1,4 @@
-from bit_battles.challenges.models import DailyChallengeStatistic, ChallengeStatistic
+from bit_battles.challenges.models import DailyChallengeStatistic, ChallengeStatistic, DailyChallenge, Challenge
 from bit_battles.utils.functions import get_back_url
 from bit_battles.utils.circuit import Circuit
 
@@ -6,6 +6,7 @@ from flask_login import login_required, current_user
 from flask import Blueprint, render_template, redirect, request
 
 import typing as t
+import json
 
 
 circuits_blueprint = Blueprint("circuits", __name__, url_prefix="/app/circuit")
@@ -21,7 +22,11 @@ def daily_circuits(id):
     if not DailyChallengeStatistic.query.filter_by(user_id=current_user.id, date=circuit["daily_id"], passed=True).first():
         return redirect(get_back_url(request))
 
-    return render_template("circuits/circuit.html", circuit=circuit["circuit"])
+    daily_challenge: t.Optional[DailyChallenge] = DailyChallenge.query.filter_by(date=circuit["daily_id"]).first()
+    if not daily_challenge:
+        return redirect(get_back_url(request))
+    
+    return render_template("circuits/circuit.html", circuit=circuit["circuit"], truthtable=json.loads(daily_challenge.truthtable))
 
 
 @circuits_blueprint.get("/challenge/<string:id>")
@@ -33,5 +38,9 @@ def challenge_circuits(id):
     
     if not ChallengeStatistic.query.filter_by(user_id=current_user.id, challenge_id=circuit["challenge_id"], passed=True).first():
         return redirect(get_back_url(request))
+    
+    challenge: t.Optional[Challenge] = Challenge.query.get(circuit["challenge_id"])
+    if not challenge:
+        return redirect(get_back_url(request))
 
-    return render_template("circuits/circuit.html", circuit=circuit["circuit"])
+    return render_template("circuits/circuit.html", circuit=circuit["circuit"], truthtable=json.loads(challenge.truthtable))
--- bit_battles/static/js/circuit.js
+++ bit_battles/static/js/circuit.js
@@ -1,3 +1,5 @@
+let truthtableElement;
+
 if (document.readyState !== 'loading') {
     circuitInit();
 } else {
@@ -6,15 +8,57 @@ if (document.readyState !== 'loading') {
     });
 }
 
-
 function circuitInit() {
     debugMode();
 
+    truthtableElement = document.getElementById("truthtable");
+    loadTruthtable(truthtable);
     loadGates(circuit.g);
     loadWires(circuit.w);
 
     drawGrid();
     drawCanvas();
+}
+
+function loadTruthtable(data) {
+    const testColumn = document.getElementById("test-column");
+    let firstOutput = false;
+    let columnLength = 0;
+
+    for (const [key, values] of Object.entries(data)) {
+        const column = document.createElement("div");
+        const title = document.createElement("div");
+        title.classList.add("title")
+        title.innerText = key
+
+        column.appendChild(title);
+        values.forEach(value => {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.classList.add(value? "on": "off");
+            cell.innerText = value;
+
+            column.appendChild(cell);
+        });
+        
+        if (key.charCodeAt(0) > 77 && !firstOutput) {
+            column.classList.add("output");
+            firstOutput = true;
+        }
+
+        truthtableElement.appendChild(column);
+        columnLength = values.length;
+    }
+
+    for (let i = 0; i < columnLength; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.classList.add("passed");
+        cell.innerText = "v";
+        cell.id = `test-${i}`;
+
+        testColumn.appendChild(cell);
+    }
 }
 
 function loadGates(gates) {
