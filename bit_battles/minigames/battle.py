f3de26ffaa15387f927e640ecf093d02a738e013
kipteamm <toro.een@gmail.com>
2025-08-23 15:21:38 +0200

    Split minigames up into separate files Removed redundant db models

--- bit_battles/minigames/battle.py
+++ bit_battles/minigames/battle.py
@@ -1,5 +1,3 @@
-from bit_battles.minigames.truthtables import TableGenerator
-from bit_battles.utils.functions import format_seconds
 from bit_battles.auth.models import User
 
 from functools import wraps
@@ -174,99 +172,6 @@ class Battle:
             data[field] = getattr(self, field)
         
         return data
-
-
-class Gates(Enum):
-    AND = 0
-    OR = 1
-    NOT = 2
-    XOR = 4
-    
-
-class CCPlayer(Player):
-    def __init__(self, user: User, battle: "Battle") -> None:
-        super().__init__(user, battle)
-
-        self.gates = 0
-        self.longest_path = 0
-
-    def reset(self) -> None:
-        super().reset()
-
-        self.gates = 0
-        self.longest_path = 0
-
-    @require_registration
-    def get_message(self, state: SubmissionState) -> str:
-        if state == SubmissionState.PASSED:
-            return f"{self.username} finished in {format_seconds(self.submission_on - self.battle.started_on)} with {self.gates} gate{'' if self.gates == 1 else 's'} and a longest path of {self.longest_path}."
-        
-        raise NotImplementedError
-
-
-class CircuitClash(Battle):
-    def __init__(self, owner_id: str, private: bool, inputs: int, outputs: int, gates: list[Gates]) -> None:
-        """
-        owner_id :user id: of the game host
-        inputs :int: amount of input gates
-        outputs :int: amount of output gates
-        gates :list[Gates]: 
-
-        Generates a truthtable with the given settings
-        """
-        super().__init__(owner_id, private)
-        self.inputs = inputs
-        self.outputs = outputs
-        self.gates = gates
-        self.truthtable = {}
-
-        manager.add_battle(self)
-
-    @require_registration
-    def add_player(self, user: User) -> None:
-        player = CCPlayer(user, self)
-
-        self.players.append(player)
-        manager.add_player(player)
-
-    @require_registration
-    def start(self) -> None:
-        self.truthtable = TableGenerator(self.inputs, self.outputs, None).table
-
-    @require_registration
-    def submit(self, player: Player, data: dict[str, t.Any]) -> tuple[SubmissionState, str | None]:
-        gates, wires = data.get("gates"), data.get("wires")
-        if not gates or not wires:
-            return SubmissionState.ERROR, "Missing circuit data."
-        
-        try:
-            passed, longest_path = Simulate(
-                gates, 
-                wires,
-                {}
-                ).test(json.loads(battle.truthtable))
-
-            gates_used = len(gates) - battle.inputs - battle.outputs
-
-            player.gates = gates_used
-            player.longest_path = longest_path
-
-            player.submission_on = time.time()
-            player.passed = passed
-
-
-        except Exception as e:
-            db.session.commit()
-            return {"error": str(e)}, 400
-
-        if not passed:
-            return SubmissionState.FAILED, None
-        
-        success, id = Circuit(gates, wires).save("battle", battle.id, player.user_id)
-        if success:
-            player.circuit_id = id
-
-        return SubmissionState.PASSED, None
 
 
 manager = BattleManager()
