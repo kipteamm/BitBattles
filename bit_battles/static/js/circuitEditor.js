befd3dc3bdf317f6ede45dbd18bc9899a0ea1414
kipteamm <toro.een@gmail.com>
2025-08-24 17:04:51 +0200

    Added connector setup

--- bit_battles/static/js/circuitEditor.js
+++ bit_battles/static/js/circuitEditor.js
@@ -1,8 +1,9 @@
 "use strict";
 window.onload = () => {
     const editor = new Editor("editor");
-    editor.registerPlaceable(new Placeable(new Square("#ffcc00", 3), "AND"));
-    const state = new Placeable(new Circle("#9b9b9bff", 1.5), "STATE");
+    const AND = new Placeable(new Square("#ffcc00", 3), "AND", { top: 0, left: 0, radius: .20, align: false, color: "#000" }, { top: 1, left: 0, radius: .20, align: false, color: "#000" }, { top: 2, left: 0, radius: .20, align: false, color: "#000" }, { top: 1, left: 3, radius: .20, align: false, color: "#000" });
+    editor.registerPlaceable(AND);
+    const state = new Placeable(new Circle("#818181", 1.5), "STATE", { top: 0, left: 0, radius: 1.5, align: true, color: null });
     editor.registerPlaceable(state);
     state.listen("place", placeState);
     function placeState(placed) {
--- bit_battles/static/ts/circuitEditor.ts
+++ bit_battles/static/ts/circuitEditor.ts
@@ -1,18 +1,24 @@
 window.onload = () => {
 const editor = new Editor("editor");
 
-editor.registerPlaceable(
-    new Placeable(new Square(
-        "#ffcc00",
-        3
-    ), "AND")
+const AND = new Placeable(
+    new Square("#ffcc00", 3), 
+    "AND",
+    { top: 0, left: 0, radius: .20, align: false, color: "#000" },
+    { top: 1, left: 0, radius: .20, align: false, color: "#000" },
+    { top: 2, left: 0, radius: .20, align: false, color: "#000" },
+    { top: 1, left: 3, radius: .20, align: false, color: "#000" },
 );
+editor.registerPlaceable(AND);
 
-const state = new Placeable(new Circle(
-        "#9b9b9bff",
-        1.5
-    ), "STATE")
+
+const state = new Placeable(
+    new Circle("#818181", 1.5), 
+    "STATE",
+    { top: 0, left: 0, radius: 1.5, align: true, color: null },
+);
 editor.registerPlaceable(state);
+
 state.listen("place", placeState);
 
 function placeState(placed: Placed) {
